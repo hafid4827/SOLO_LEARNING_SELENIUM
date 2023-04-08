fb22a1db0db84db7166d9c190b6c2c2598114d50
Hafid <luceres12joshua@gmail.com>
2023-04-08 17:51:00 -0500

    more example

--- tests/text_example_learning.py
+++ tests/text_example_learning.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
-
 from os.path import abspath
 from time import sleep
 
@@ -12,8 +11,8 @@ driver = webdriver.Firefox(service=services)
 
 driver.fullscreen_window()  # esto hace grande la pantalla
 
-# nos redirecciona a el sitio pasado como argumento al paarmetro tal de tipo strgin y una vez abierto nuestro navegador atravz del dirver correspondiente al navegador usado,
-driver.get("https://start.garudalinux.org/") 
+# Nos redirecciona a el sitio pasado como argumento al parmetro tal de tipo strgin y una vez abierto nuestro navegador atravz del dirver correspondiente al navegador usado,
+driver.get("https://start.garudalinux.org/")
 sleep(1)
 
 
@@ -41,3 +40,49 @@ iter_select_button_redirect = dict(
 )
 
 init_render(button_redirect=iter_select_button_redirect)
+
+
+
+# no se comenta
+
+# input_path = "/html/body/div/div/div/div/nav/div/div[2]/div[2]/div/input"
+""" input_use = driver.find_element(by=By.XPATH, value=input_path)
+
+list_letter = "hola mundo"
+for item_list_letter in list_letter:
+    input_use.send_keys(item_list_letter)
+    sleep(1)
+
+sleep(2)
+input_use.clear()
+sleep(2)
+
+ """
+
+
+""" p_info_title = "/html/body/div/div/section/div/div/div/div[3]/div[1]/div/a/div/div[1]/div[2]/p[1]"
+p_info_text = "/html/body/div/div/section/div/div/div/div[3]/div[1]/div/a/div/div[1]/div[2]/p[2]"
+
+p_scraping_a = driver.find_element(by=By.XPATH, value=p_info_title)
+p_scraping_b = driver.find_element(by=By.XPATH, value=p_info_text)
+
+p_scraping_text_a = p_scraping_a.text
+p_scraping_text_b = p_scraping_b.text
+
+print("titulo es => ", p_scraping_text_a,
+      "\ninformacion es =>", p_scraping_text_b)
+
+ """
+""" list_element_Xpath = "/html/body/div/div/section/div/div/div/div[3]/div"
+list_web_elemets = driver.find_elements(by=By.XPATH, value=list_element_Xpath)
+
+for item in list_web_elemets:
+    text_result = item.text
+    print(text_result)
+    print('#' * 25)
+
+print(type(list_web_elemets), "=>", len(list_web_elemets))
+
+sleep(2)
+driver.close()
+ """
