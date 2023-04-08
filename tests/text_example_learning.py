409155ece6362bc8c13b4595faacce220babc644
Hafid <luceres12joshua@gmail.com>
2023-04-08 16:01:41 -0500

    iter function

--- tests/text_example_learning.py
+++ tests/text_example_learning.py
@@ -16,11 +16,28 @@ driver.fullscreen_window()  # esto hace grande la pantalla
 driver.get("https://start.garudalinux.org/")
 sleep(1)
 
-path_button_one = "/html/body/div/div/div/div/nav/div/div[2]/div[1]/a[1]"
-user_use = driver.find_element(by=By.XPATH, value=path_button_one)
-sleep(1)
 
-user_use.click() # se usara 3 veces x cada redireccion de la pagina
-sleep(5)
+def init_render(button_redirect: dict) -> None:
+    for item_button_redirect in button_redirect:
+        path_button_redirect = button_redirect[item_button_redirect]
+        clik_redirect_to(
+            path_button_redirect=path_button_redirect,
+            time_sleep=2,
+        )
+    else:
+        driver.close()
 
-driver.close()
+
+def clik_redirect_to(path_button_redirect: str, time_sleep: int = 3) -> None:
+    user_use = driver.find_element(by=By.XPATH, value=path_button_redirect)
+    user_use.click()  # se usara 3 veces x cada redireccion de la pagina
+    sleep(time_sleep)  # espera que la pagina cargue
+
+
+iter_select_button_redirect = dict(
+    path_button_one="/html/body/div/div/div/div/nav/div/div[2]/div[1]/a[1]",
+    other_click_download="/html/body/div[2]/span/a[1]",
+    other_click_donate="/html/body/div[3]/span/a[4]",
+)
+
+init_render(button_redirect=iter_select_button_redirect)
